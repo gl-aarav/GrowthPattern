8e6f6ff85e02c4db835a61a296df8f414c45b18b
Aarav Goyal <gl.aarav@gmail.com>
2025-01-13 15:00:46 -0800

    Add files via upload

---
+++ GrowthPattern.java
@@ -0,0 +1,52 @@
+/*
+ * Aarav Goyal
+ * 1/13/2025
+ * GrowthPattern.java
+ * 
+ * Pseudocode:
+	Gets an array of numbers of height of the plant starting from 0
+	Reads in the array
+	Adds the height to 5
+	Prints 'o' for first time the result and no change in height
+	Prints '-' for the decreased value
+	
+*/
+public class GrowthPattern
+{
+	
+	int height[] = {-1,-3,1,2,3,4,3,4,4,6,8,6,6,7,10};
+	public GrowthPattern()
+	{
+		
+	}
+	
+	public static void main (String [] agrs)
+	{
+		GrowthPattern gp = new GrowthPattern();
+		gp.growIt();
+	}
+	
+	public void growIt()
+	{
+		for (int i = 0; i<height.length; i++)
+		{
+			height
+		}
+	}
+	
+	public void decideSymbols()
+	{
+		
+	}
+	
+	public void printSymbols()
+	{
+		
+	}
+	
+	public void printRate()
+	{
+		
+	}
+	
+}
